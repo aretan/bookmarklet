57e7542adfefdc6dc1ff4975a2307037967ec561
あれたん <alex_sakura@hotmail.com>
2019-03-09 11:27:58 +0900

    Update google.js

--- google.js
+++ google.js
@@ -1,24 +1,13 @@
 (function(){
-  var ele = document.createElement('div'); 
-  ele.style.backgroundColor = 'red';
-  
   var xhr = new XMLHttpRequest();
+  var ifr = document.createElement("iframe");
+
   var req = document.querySelectorAll(".srg .g .r h3");
   for(var i in req){
     if(req[i].innerText){
-      xhr.open("GET", req[i].parentNode.getAttribute('href'), true);
       console.log(req[i].parentNode.getAttribute('href'));
-      xhr.onload = function (e) {
-        if (xhr.readyState === 4) {
-          ele.innerHTML = xhr.status;
-          e.parentNode.appendChild(ele);
-        }
-      }
-      xhr.onerror = function (e) {
-        ele.innerHTML = xhr.statusText;
-        e.parentNode.appendChild(ele);
-      };
-      xhr.send(null);
+      ifr.src = req[i].parentNode.getAttribute('href');
+      req[i].parentNode.appendChild(ifr);
     }
   }
 })();
